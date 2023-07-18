f3429d8d4a94414feaba9144b94659afba9f40aa
WojciechKuz <104651306+WojciechKuz@users.noreply.github.com>
2023-07-18 23:25:34 +0200

    step0 (choose camera or gallery)

---
+++ app/src/main/java/com/student/mappic/addmap/Step0Fragment.kt
@@ -0,0 +1,43 @@
+package com.student.mappic.addmap
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.student.mappic.R
+import com.student.mappic.databinding.FragmentStep0Binding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Step0Fragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Step0Fragment : Fragment() {
+
+    private var _binding: FragmentStep0Binding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentStep0Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // set onclicklisteners here
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

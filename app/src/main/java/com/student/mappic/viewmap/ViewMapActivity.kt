1721d17f764abbca8d822d95bbe4b99143ca4060
WojciechKuz <104651306+WojciechKuz@users.noreply.github.com>
2023-07-23 20:06:35 +0200

    View Map Activity

---
+++ app/src/main/java/com/student/mappic/viewmap/ViewMapActivity.kt
@@ -0,0 +1,16 @@
+package com.student.mappic.viewmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.student.mappic.R
+
+/**
+ * This activity shows user map and displays user's location on it
+ */
+class ViewMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_map)
+    }
+    // TODO show map & user position
+}

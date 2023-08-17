51fff3ac0745b0554aa9e203bf7cf21aa4654e5b
WojciechKuz <104651306+WojciechKuz@users.noreply.github.com>
2023-08-17 23:48:10 +0200

    trash

---
+++ not-used/ImgGetQuery.kt.txt.kt
@@ -0,0 +1,83 @@
+import android.content.ContentResolver
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+
+class `ImgGetQuery.kt.txt` {
+    fun getImage(uri: Uri) {
+        // Need the READ_EXTERNAL_STORAGE permission if accessing video files that your app didn't create.
+
+        // Container for information about each video.
+        data class Images(
+            val uri: Uri,
+            val name: String,
+            val width: Int,
+            val height: Int,
+            val orientation: Int,
+            val size: Int
+        )
+        val imagesList = mutableListOf<Images>()
+
+        val collection =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                MediaStore.Images.Media.getContentUri(
+                    MediaStore.VOLUME_EXTERNAL
+                )
+            } else {
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            }
+
+        val projection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DISPLAY_NAME,
+            MediaStore.Images.Media.WIDTH,
+            MediaStore.Images.Media.HEIGHT,
+            MediaStore.Images.Media.ORIENTATION,
+            MediaStore.Images.Media.SIZE
+        )
+
+// Show only videos that are at least 5 minutes in duration.
+        val selection = "${MediaStore.Images.Media._ID} == ?"
+        val selectionArgs = arrayOf(
+            uri
+        )
+
+// Display videos in alphabetical order based on their display name.
+        val sortOrder = "${MediaStore.Video.Media.DISPLAY_NAME} ASC"
+
+        val query = ContentResolver.query(
+            collection,
+            projection,
+            selection,
+            selectionArgs,
+            sortOrder
+        )
+        query?.use { cursor ->
+            // Cache column indices.
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID)
+            val nameColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME)
+            val durationColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION)
+            val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE)
+
+            while (cursor.moveToNext()) {
+                // Get values of columns for a given video.
+                val id = cursor.getLong(idColumn)
+                val name = cursor.getString(nameColumn)
+                val duration = cursor.getInt(durationColumn)
+                val size = cursor.getInt(sizeColumn)
+
+                val contentUri: Uri = ContentUris.withAppendedId(
+                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
+
+                // Stores column values and the contentUri in a local object
+                // that represents the media file.
+                imagesList += Images(contentUri, name, /* TODO */)
+            }
+        }
+
+    }
+}

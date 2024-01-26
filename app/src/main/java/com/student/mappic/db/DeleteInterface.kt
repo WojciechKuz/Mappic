951d19f1be464319466394c4053bf8ff5124a355
WojciechKuz <104651306+WojciechKuz@users.noreply.github.com>
2024-01-26 13:40:56 +0100

    database schema, problem story

--- app/src/main/java/com/student/mappic/db/DeleteInterface.kt
+++ app/src/main/java/com/student/mappic/db/DeleteInterface.kt
@@ -5,3 +5,17 @@ import android.content.Context
 fun interface DeleteInterface {
     fun delete(context: Context, mapid: Long)
 }
+
+/* A problem story:
+    This package used to be named com.student.mappic.DB
+
+    When I renamed it (refactor) to com.student.mappic.db,
+    and tried to build project errors like "unresolved reference to <anything that is in this package>"
+    occurred.
+
+    To fix it I selected Build > Clean Project,
+    also I deleted content of Mappic/app/schema directory (database schema),
+    because element in it had old package name.
+
+    Then I just build/rebuild project and everything was fine.
+ */

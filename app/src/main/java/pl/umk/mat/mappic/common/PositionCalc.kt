790387a67b4fa4e54dd17feb84cb69b954c1ed8f
WojciechKuz <104651306+WojciechKuz@users.noreply.github.com>
2024-01-27 14:50:07 +0100

    basic_whereUser algorithm

--- app/src/main/java/pl/umk/mat/mappic/common/PositionCalc.kt
+++ app/src/main/java/pl/umk/mat/mappic/common/PositionCalc.kt
@@ -1,10 +1,13 @@
 package pl.umk.mat.mappic.common
 
+import android.graphics.Point
+import android.graphics.PointF
 import pl.umk.mat.mappic.db.MPoint
 import kotlin.math.PI
 import kotlin.math.abs
 import kotlin.math.cos
 import kotlin.math.pow
+import kotlin.math.round
 import kotlin.math.sqrt
 
 /**
@@ -15,7 +18,65 @@ import kotlin.math.sqrt
  * [medium](https://blog.mapbox.com/fast-geodesic-approximations-with-cheap-ruler-106f229ad016)
  *
  */
-class PositionCalc {
+class PositionCalc(private val px: Array<Point>, private val geo: Array<PointF>) {
+
+    init {
+        if(px.size >= 2) { // Equal or greater is ok, anything over 2 will be ignored.
+            throw IndexOutOfBoundsException("px array size must be 2!")
+        }
+        if(geo.size >= 2) {
+            throw IndexOutOfBoundsException("geo array size must be 2!")
+        }
+    }
+    private val geoDiff = PointF(
+        geo[1].x - geo[0].x,
+        geo[1].y - geo[0].y
+    )
+    private val pxDiff = Point(
+        px[1].x - px[0].x,
+        px[1].y - px[0].y
+    )
+
+    constructor(mPoints: Array<MPoint>) : this(
+        getPxArray(mPoints),
+        getGpsArray(mPoints)
+    ) {
+        if(mPoints.size >= 2) {
+            throw IndexOutOfBoundsException("mPoint array size must be 2!")
+        }
+    }
+
+    // TODO polar update (so it handles coordinates on north pole)
+    /**
+     * Calculate pixel user position on (original) image, based on geolocation.
+     * @param geoUserP user geoposition
+     * These are steps performed to calculate:\
+     * relGeo1 = geoUserP - geo1    \
+     * geoScale = relGeo1 / geoDiff \
+     * relPx1 = geoScale * pxDiff   \
+     * pxUserP = relPx1 + px1
+     * @return pixel user position
+     */
+    fun basic_whereUser(geoUserP: PointF): Point {
+        val relative2Geo1 = PointF(
+            geo[0].x - geoUserP.x,
+            geo[0].y - geoUserP.y
+        )
+        val geoScale = PointF(
+            relative2Geo1.x / geoDiff.x,
+            relative2Geo1.y / geoDiff.y
+        )
+        val relative2px1 = PointF(
+            geoScale.x * pxDiff.x.toFloat(),
+            geoScale.y * pxDiff.y.toFloat()
+        )
+        val pxUserP = Point(
+            round(relative2px1.x + px[0].x).toInt(),
+            round(relative2px1.y + px[0].y).toInt()
+        )
+        return pxUserP
+    }
+
     companion object {
         /** Earth sphere radius in meters. Mean Earth radius 6371.0088 km defined in WGS 84. */
         val EARTH_RADIUS = 6371008.8
@@ -37,6 +98,7 @@ class PositionCalc {
         fun curvatureLength(deg: Double, radius: Double): Double { return radius * toRad(deg) }
 
         // TODO test it:        Also, what precision it has?
+        // This would be harder in polar update
         /**
          * Calculates distance between two points on Earth's surface. Unit - meters.
          *
@@ -45,12 +107,25 @@ class PositionCalc {
         fun geoPosToDist(a: MPoint, b: MPoint): Double {
             val degDistNS = abs(b.ygps - a.ygps)
             val degDistEW = abs(b.xgps - a.xgps)
-            val meanLatit = meanLatitude(a.ygps, b.ygps)
+            val meanLatit = meanLatitude(a.ygps, b.ygps) // aka average latitude
             val MdistNS = curvatureLength(degDistNS, LATIT_RADIUS)
             val MdistEW = curvatureLength(degDistEW, radiusAtLatitude(meanLatit))
 
             // ...and Pythagoras:
             return sqrt(MdistNS.pow(2) + MdistEW.pow(2))
+        }
+
+        private fun getPxArray(mPoints: Array<MPoint>): Array<Point> {
+            return arrayOf(
+                Point(mPoints[0].xpx, mPoints[0].ypx),
+                Point(mPoints[1].xpx, mPoints[1].ypx)
+            )
+        }
+        private fun getGpsArray(mPoints: Array<MPoint>): Array<PointF> {
+            return arrayOf(
+                PointF(mPoints[0].xgps.toFloat(), mPoints[0].ygps.toFloat()),
+                PointF(mPoints[1].xgps.toFloat(), mPoints[1].ygps.toFloat())
+            )
         }
     }
 }
